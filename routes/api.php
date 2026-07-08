10690272c5ea91779d4e4ee87918ebab1d56ed16
zidan <zidanemuh650@gmail.com>
2026-07-08 23:31:32 +0700

    Add CRUD2

--- routes/api.php
+++ routes/api.php
@@ -6,19 +6,24 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AntreanController;
 use App\Http\Controllers\LayananController;
 use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\StokBarangController;
 use App\Http\Controllers\PerangkatPrinterController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/layanan', [LayananController::class, 'index']);
-Route::get('/antrean', [AntreanController::class, 'index']);
-Route::get('/master-data', [TransaksiController::class, 'getMasterData']);
-Route::get('/printer', [PerangkatPrinterController::class, 'index']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/layanan', [LayananController::class, 'index']);
+    Route::get('/antrean', [AntreanController::class, 'index']);
+    Route::get('/operator', [OperatorController::class, 'index']);
+    Route::get('/stok-barang', [StokBarangController::class, 'index']);
+    Route::get('/printer', [PerangkatPrinterController::class, 'index']);
+    Route::get('/master-data', [TransaksiController::class, 'getMasterData']);
+
     // Manajemen Transaksi
     Route::post('/transaksi', [TransaksiController::class, 'create']);
     Route::put('/transaksi/{id}', [TransaksiController::class, 'update']);
@@ -32,8 +37,16 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/printer/{id}', [PerangkatPrinterController::class, 'update']);
     Route::delete('/printer/{id}', [PerangkatPrinterController::class, 'delete']);
 
-    // Manajemen Stok Barang?
-    // Manajemen Operator?
+    // Manajemen Stok Barang
+    Route::post('/stok-barang', [StokBarangController::class, 'create']);
+    Route::put('/stok-barang/{id}', [StokBarangController::class, 'update']);
+    Route::delete('/stok-barang/{id}', [StokBarangController::class, 'delete']);
+
+    // Manajemen Operator
+    Route::post('/operator', [OperatorController::class, 'create']);
+    Route::put('/operator/{id}', [OperatorController::class, 'update']);
+    Route::delete('/operator/{id}', [OperatorController::class, 'delete']);
+    
     // Manajemen Layanan?
     // Manajemen Pelanggan?
 });
